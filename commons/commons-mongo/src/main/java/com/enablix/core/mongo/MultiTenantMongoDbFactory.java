cc8d236f17b39e227c19d428a0c037491538f40e
dikshitluthra <dikshit_luthra@yahoo.com>
2015-03-08 22:34:43 +0530

    Multi tenant mongo db factory

---
+++ commons/commons-mongo/src/main/java/com/enablix/core/mongo/MultiTenantMongoDbFactory.java
@@ -0,0 +1,30 @@
+package com.enablix.core.mongo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+import com.enablix.commons.util.tenant.TenantUtil;
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+
+public class MultiTenantMongoDbFactory extends SimpleMongoDbFactory {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(MultiTenantMongoDbFactory.class);
+	
+	public MultiTenantMongoDbFactory(Mongo mongo, String databaseName) {
+		super(mongo, databaseName);
+	}
+	
+	@Override 
+	public DB getDb(String dbName) {
+		return super.getDb(getTenantAwareDbName(dbName));
+	}
+	
+	private String getTenantAwareDbName(String dbName) {
+		String tenantId = TenantUtil.getTenantId();
+		LOGGER.debug("Tenant id : {}", tenantId);
+		return tenantId == null ? dbName : tenantId + "_" + dbName;
+	}
+
+}
---
+++ commons/commons-utility/src/main/java/com/enablix/commons/util/tenant/TenantUtil.java
@@ -0,0 +1,9 @@
+package com.enablix.commons.util.tenant;
+
+public class TenantUtil {
+
+	public static String getTenantId() {
+		return "test";
+	}
+	
+}
