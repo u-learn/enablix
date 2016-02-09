dafad667624e451e01ed3685550ce09a5eafab80
Dikshit Luthra <dikshit_luthra@yahoo.com>
2016-02-09 18:31:57 +0530

    corrected ui asset path

--- services/enablix-app/ext-resources/startup-local.sh
+++ services/enablix-app/ext-resources/startup-local.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 clear
 echo "Launching Enablix app."
-java -Xms64m -Xmx128m -DbaseDir=./../../ext-resources -Dui.assets.external.paths=./../../../../web/studio -DdataDir=. -Dlog4j.configuration=file:"./config/properties/log4j.properties" -jar enablix-app-1.0-SNAPSHOT.jar --server.port=8080
+java -Xms64m -Xmx128m -DbaseDir=./../../ext-resources -Dui.assets.external.paths=./../../../../web/studio/ -DdataDir=. -Dlog4j.configuration=file:"./config/properties/log4j.properties" -jar enablix-app-1.0-SNAPSHOT.jar --server.port=8080
