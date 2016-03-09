1896fdb48955ff0aef6ac8ebb2b3821025e2cc6c
Richa Agrawal <qir.richa.agrawal@gmail.com>
2016-03-09 17:56:03 +0530

    back button in studio

--- web/studio/widgets/directive/focus/focusDirective.js
+++ web/studio/widgets/directive/focus/focusDirective.js
@@ -15,4 +15,14 @@ function($timeout) {
  }
 };
 }); 
-   
+
+enablix.studioApp.directive('back', ['$window', function($window) {
+        return {
+            restrict: 'A',
+            link: function (scope, elem, attrs) {
+                elem.bind('click', function () {
+                    $window.history.back();
+                });
+            }
+        };
+    }]);
