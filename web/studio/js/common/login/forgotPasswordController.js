03de67fad23d70f75bdace0cce47e19f999061db
Richa Agrawal <qir.richa.agrawal@gmail.com>
2016-03-17 11:25:10 +0530

    email template changes

--- web/studio/js/common/login/forgotPasswordController.js
+++ web/studio/js/common/login/forgotPasswordController.js
@@ -2,26 +2,12 @@ enablix.studioApp.controller('ForgotPasswordController',
 			['$scope', '$state', 'RESTService','UserService', '$rootScope', 'StateUpdateService',
 	function( $scope,   $state,   RESTService,  UserService, $rootScope,   StateUpdateService) {
 		$scope.userData={};
-
-
 		$scope.resetPassword = function() {
-			/* if($scope.userData.confirm===$scope.passwordData.password){
-				UserService.updatepassword($scope.passwordData.password);
-			} */
 			RESTService.postForData('resetpassword',null,$scope.userData.userId, null,function(status) {
 				if(status)
-					StateUpdateService.goToLogin();
-						/* Notification.primary({message: "Reset mail sent successfully!", delay: enablix.errorMsgShowTime});
-				else
-						Notification.error({message: "Error sending mail ", delay: enablix.errorMsgShowTime}); */
-				
-					}, function(errorObj) {    		
-						Notification.error({message: "Error sending mail ", delay: enablix.errorMsgShowTime});
-					});	
-			
-				//UserService.sendMail(null,$scope.userData.userId,"forgotpassword");
-			
-				
+					StateUpdateService.goToLogin();				
+				}, function(errorObj) {  		
+					});					
 		}
 		
 	}
