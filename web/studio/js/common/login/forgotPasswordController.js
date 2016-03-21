3444fbeac272c7934e3136e97256674baee822fc
Richa Agrawal <qir.richa.agrawal@gmail.com>
2016-03-21 12:34:36 +0530

    fix for error while clicking forgotPassword confirm button

--- web/studio/js/common/login/forgotPasswordController.js
+++ web/studio/js/common/login/forgotPasswordController.js
@@ -1,6 +1,6 @@
 enablix.studioApp.controller('ForgotPasswordController', 
-			['$scope', '$state', 'RESTService','UserService', '$rootScope', 'StateUpdateService',
-	function( $scope,   $state,   RESTService,  UserService, $rootScope,   StateUpdateService) {
+			['$scope', '$state', 'RESTService', '$rootScope', 'StateUpdateService',
+	function( $scope,   $state,   RESTService,  $rootScope,   StateUpdateService) {
 		$scope.userData={};
 		$scope.resetPassword = function() {
 			RESTService.postForData('resetpassword',null,$scope.userData.userId, null,function(status) {
