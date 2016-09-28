8996eabef6fc6c98ef00a4ebd7ef07cbf5b9cb0e
Dikshit Luthra <dikshit_luthra@yahoo.com>
2016-09-28 16:13:21 +0530

    added to print entity create/update data via service API

--- services/enablix-app/src/main/java/com/enablix/app/web/EntityUpdateController.java
+++ services/enablix-app/src/main/java/com/enablix/app/web/EntityUpdateController.java
@@ -3,6 +3,8 @@ package com.enablix.app.web;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +17,17 @@ import com.enablix.commons.util.id.IdentityUtil;
 @RestController
 @RequestMapping("/services/entity")
 public class EntityUpdateController {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(EntityUpdateController.class);
 
 	@RequestMapping(method = RequestMethod.POST, value="/{transformer}/c/{containerQId}/",
 			produces = "application/json")
 	public Map<String, Object> createOrUpdateEntity(@RequestBody Map<String, Object> data,
 			@PathVariable String transformer, @PathVariable String containerQId) {
 		// TODO:
+		
+		LOGGER.debug("Entity Create/Update request: {}", data);
+		
 		Map<String, Object> returnData = new HashMap<String, Object>();
 		returnData.put(ContentDataConstants.IDENTITY_KEY, IdentityUtil.generateIdentity(returnData));
 		return returnData;
