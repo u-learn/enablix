9e81462689013382e7ccf0dd7767e6cd68f247ad
amjayee <amit.jayee@gmail.com>
2016-12-14 14:32:33 +0530

    Date convertion
    Used JsonDateSerializer to serialize Date

--- commons/commons-domain/src/main/java/com/enablix/core/domain/activity/ActivityAudit.java
+++ commons/commons-domain/src/main/java/com/enablix/core/domain/activity/ActivityAudit.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.enablix.core.domain.BaseDocumentEntity;
+import com.enablix.core.ui.JsonDateSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 @Document(collection = "ebx_activity_audit")
 public class ActivityAudit extends BaseDocumentEntity {
@@ -32,7 +34,13 @@ public class ActivityAudit extends BaseDocumentEntity {
 	public void setChannel(ActivityChannel channel) {
 		this.channel = channel;
 	}
-
+	/*
+	 * Added by Amit Jayee 14th Dec
+	 */
+	@JsonSerialize(using=JsonDateSerializer.class)
+	/*
+	 * Added by Amit Jayee 14th Dec
+	 */
 	public Date getActivityTime() {
 		return activityTime;
 	}
