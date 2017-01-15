48c8d5632213b52a458fdcdad5bf7eea28545b08
amjayee <amit.jayee@gmail.com>
2017-01-16 00:16:12 +0530

    Content Download URL Copy

---
+++ commons/commons-domain/src/main/java/com/enablix/core/domain/activity/ContentDownldURLCopy.java
@@ -0,0 +1,52 @@
+package com.enablix.core.domain.activity;
+
+public class ContentDownldURLCopy extends ContentActivity{
+
+
+
+	private String contextName;
+	
+	private String contextId; 
+	
+	private String contextTerm;
+	
+	protected ContentDownldURLCopy() {
+	}
+	
+	public ContentDownldURLCopy(String itemIdentity, String containerQId, ContainerType containerType, String itemTitle) {
+		this(itemIdentity, containerQId, containerType, null, null, null, itemTitle);
+	}
+
+	public ContentDownldURLCopy(String itemIdentity, String containerQId, ContainerType containerType,
+			String contextName, String contextId, String contextTerm, String itemTitle) {
+		super(itemIdentity, containerQId, containerType, ContentActivityType.CONTENT_DOWNLD_URL_COPIED, itemTitle);
+		this.contextName = contextName;
+		this.contextId = contextId;
+		this.contextTerm = contextTerm;
+	}
+
+	public String getContextName() {
+		return contextName;
+	}
+
+	public void setContextName(String campaign) {
+		this.contextName = campaign;
+	}
+
+	public String getContextId() {
+		return contextId;
+	}
+
+	public void setContextId(String contextId) {
+		this.contextId = contextId;
+	}
+
+	public String getContextTerm() {
+		return contextTerm;
+	}
+
+	public void setContextTerm(String contextTerm) {
+		this.contextTerm = contextTerm;
+	}
+
+}
