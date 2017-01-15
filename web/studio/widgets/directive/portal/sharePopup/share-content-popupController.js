cda9c0792758e33d6c360af32238221a9bfb4091
amjayee <amit.jayee@gmail.com>
2017-01-15 20:08:50 +0530

    Auto Suggest Email and Sample Message
    Auto Suggest Email and Sample Message as Email

--- web/studio/widgets/directive/portal/sharePopup/share-content-popupController.js
+++ web/studio/widgets/directive/portal/sharePopup/share-content-popupController.js
@@ -1,29 +1,75 @@
 enablix.studioApp.controller('ShareController', [
-             '$scope', '$stateParams', '$modalInstance', '$timeout', 'containerQId', 'contentIdentity', 'ContentShareService', 'Notification',
-	function( $scope,   $stateParams,   $modalInstance,   $timeout,   containerQId,   contentIdentity,   ContentShareService,   Notification) {
-
-		$scope.shareContent = function() {
+	'$scope', '$stateParams', '$modalInstance', '$timeout', 'containerQId', 'contentIdentity', 'ContentShareService', 'Notification','RESTService',
+	function( $scope,   $stateParams,   $modalInstance,   $timeout,   containerQId,   contentIdentity,   ContentShareService,   Notification,RESTService) {
+		$scope.validationCheck=false;
+		var email ;
+		$scope.querySearch   = querySearch;
+		function querySearch (query) {
+			return query ? $scope.states.filter( createFilterFor(query) ) : $scope.states;
+		}
+		function validateEmail() {
 			
-			$scope.shareForm.emailid.$touched = true;
-			if ($scope.shareForm.$invalid) {
-				return;
+			if($scope.selectedItem==null)
+			{
+				email=$scope.searchText;
 			}
-			
-			ContentShareService.shareContent(containerQId, contentIdentity, $scope.emailid, function(sent) {
-				if(sent) {
-					Notification.primary({message: "Mail sent successfully", delay: enablix.errorMsgShowTime});
-				} else {
-					Notification.error({message: "Error sending mail ", delay: enablix.errorMsgShowTime});
-				}
-			}, function(fail) {
-				Notification.error({message: "Error sending mail ", delay: enablix.errorMsgShowTime});
+			else{
+				email=$scope.selectedItem.value;
+			}
+			var atpos = email.indexOf("@");
+			var dotpos = email.lastIndexOf(".");
+			if (atpos<1 || dotpos<atpos+2 || dotpos+2>=email.length) {
+				return false;
+			}
+			return true;
+		}
+		function getAllUsers(_success){
+			RESTService.getForData('systemuser', null, null, function(data) {
+				_success(data);	    	
+			}, function() {    		
+				Notification.error({message: "Error loading user data", delay: enablix.errorMsgShowTime});
 			});
-			
-			$modalInstance.close();
+		};
+		function populateUserIds(data){
+			$scope.states = data.map(function (data) {
+				return {
+					value: data.userId.toLowerCase(),
+					display: data.profile.name
+				};
+			});
+		}
+
+		function createFilterFor(query) {
+			var lowercaseQuery = angular.lowercase(query);
+
+			return function filterFn(state) {
+				return (state.value.indexOf(lowercaseQuery) === 0);
+			};
+
+		}
+		$scope.shareContent = function() {
+			var val = validateEmail();
+			if(val)
+			{
+				ContentShareService.shareContent(containerQId, contentIdentity, email,$scope.emailContent ,function(sent) {
+					if(sent) {
+						Notification.primary({message: "Mail sent successfully", delay: enablix.errorMsgShowTime});
+					} else {
+						Notification.error({message: "Error sending mail ", delay: enablix.errorMsgShowTime});
+					}
+				}, function(fail) {
+					Notification.error({message: "Error sending mail ", delay: enablix.errorMsgShowTime});
+				});
+
+				$modalInstance.close();
+			}
+			else{
+				Notification.error({message: "Please enter a valid E-Mail ID ", delay: 3000});
+			}
 		}
 
 		$scope.close = function() {
 			$modalInstance.dismiss('cancel');
 		}
-
-}]);
+		getAllUsers(populateUserIds);
+	}]);
