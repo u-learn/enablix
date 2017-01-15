6fa32a949024475e19a16d3efa3f61e2741df17d
amjayee <amit.jayee@gmail.com>
2017-01-15 20:55:08 +0530

    Email Custom Content

--- commons/commons-domain/src/main/java/com/enablix/core/api/ContentDataRecord.java
+++ commons/commons-domain/src/main/java/com/enablix/core/api/ContentDataRecord.java
@@ -7,19 +7,41 @@ import org.springframework.util.Assert;
 public class ContentDataRecord {
 
 	private String templateId;
-	
+
 	private String containerQId;
-	
+
 	private Map<String, Object> record;
 
-	public ContentDataRecord(String templateId, String containerQId, Map<String, Object> record) {
-		
+	private String emailCustomContent;
+
+	public String getEmailCustomContent() {
+		return emailCustomContent;
+	}
+
+	public void setEmailCustomContent(String emailCustomContent) {
+		this.emailCustomContent = emailCustomContent;
+	}
+	public ContentDataRecord(String templateId, String containerQId, String emailCustomContent,Map<String, Object> record) {
+
 		super();
-		
+
 		Assert.notNull(templateId);
 		Assert.notNull(containerQId);
 		Assert.notNull(record, "Content record cannot be null");
-		
+
+		this.templateId = templateId;
+		this.containerQId = containerQId;
+		this.emailCustomContent = emailCustomContent;
+		this.record = record;
+	}
+	public ContentDataRecord(String templateId, String containerQId, Map<String, Object> record) {
+
+		super();
+
+		Assert.notNull(templateId);
+		Assert.notNull(containerQId);
+		Assert.notNull(record, "Content record cannot be null");
+
 		this.templateId = templateId;
 		this.containerQId = containerQId;
 		this.record = record;
@@ -48,5 +70,5 @@ public class ContentDataRecord {
 	public void setRecord(Map<String, Object> record) {
 		this.record = record;
 	}
-	
+
 }
