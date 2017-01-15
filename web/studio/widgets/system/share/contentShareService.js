20ca323238274c7043a2e587e394342a8025794f
amjayee <amit.jayee@gmail.com>
2017-01-15 20:32:27 +0530

    Share custom body with mail
    Added the email custom body as a parameter

--- web/studio/widgets/system/share/contentShareService.js
+++ web/studio/widgets/system/share/contentShareService.js
@@ -2,11 +2,12 @@ enablix.studioApp.factory('ContentShareService',
 		[		 'RESTService', 'Notification',
 	 	function (RESTService,   Notification) {
 		
-			var shareContent = function(_containerQId, _contentIdentity, _sharedWithEmailId, _onSuccess, _onError) {
+			var shareContent = function(_containerQId, _contentIdentity, _sharedWithEmailId,emailContent, _onSuccess, _onError) {
 				
 				var shareData = {"containerQId" : _containerQId, 
 							  "contentIdentity" : _contentIdentity,
-							  "emailId" : _sharedWithEmailId};
+							  "emailId" : _sharedWithEmailId,
+							  "emailCustomContent": emailContent };
 				
 				RESTService.postForData("sharecontent", null, shareData, null, _onSuccess, _onError);
 				
