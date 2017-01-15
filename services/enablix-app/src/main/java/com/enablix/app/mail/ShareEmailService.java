8b0ecdf222a78b45249e2a636ab3a41d0e667c93
amjayee <amit.jayee@gmail.com>
2017-01-15 20:44:23 +0530

    Share Email Via Email Client

--- services/enablix-app/src/main/java/com/enablix/app/mail/ShareEmailService.java
+++ services/enablix-app/src/main/java/com/enablix/app/mail/ShareEmailService.java
@@ -1,9 +1,10 @@
 package com.enablix.app.mail;
 
 import com.enablix.app.mail.web.EmailData;
+import com.enablix.core.mail.entities.ShareEmailClientDtls;
 
 public interface ShareEmailService {
 
 	public boolean sendEmail(EmailData data);
-	
+	public ShareEmailClientDtls getEmailContent(String containerQID,String contentID);
 }
