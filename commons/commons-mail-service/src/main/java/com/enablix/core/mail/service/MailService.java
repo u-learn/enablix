c5ab2fd5f93d0ef1d1c98d4146929e828729c4ff
amjayee <amit.jayee@gmail.com>
2017-01-15 21:03:24 +0530

    Share Email Via Email Client

--- commons/commons-mail-service/src/main/java/com/enablix/core/mail/service/MailService.java
+++ commons/commons-mail-service/src/main/java/com/enablix/core/mail/service/MailService.java
@@ -3,10 +3,12 @@ package com.enablix.core.mail.service;
 import com.enablix.core.domain.config.EmailConfiguration;
 import com.enablix.core.domain.config.SMTPConfiguration;
 import com.enablix.core.domain.config.TemplateConfiguration;
+import com.enablix.core.mail.entities.ShareEmailClientDtls;
 
 
 public interface MailService {
 	public boolean sendHtmlEmail(Object objectTobeMerged, String emailid,String scenario);
+	public ShareEmailClientDtls getHtmlEmail(Object objectTobeMerged, String emailid,String scenario);
 	public EmailConfiguration addEmailConfiguration (EmailConfiguration emailConfiguration);
 	public SMTPConfiguration getSMTPConfig(String domainName);
 	public Boolean deleteEmailConfiguration(EmailConfiguration emailConfiguration);
@@ -16,6 +18,7 @@ public interface MailService {
 	Boolean deleteTemplateConfiguration(TemplateConfiguration templateConfiguration);
 	boolean sendHtmlEmail(Object objectTobeMerged, String emailid, String scenario, String bodyTemplateName,
 			String subjectTemplateName);
-
+	ShareEmailClientDtls getHtmlEmail(Object objectTobeMerged, String emailid, String scenario, String bodyTemplateName,
+			String subjectTemplateName);
 	
 }
