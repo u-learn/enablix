564f3837ce7e29ecabb65eee1e37fd3d361537e7
amjayee <amit.jayee@gmail.com>
2017-01-15 20:07:04 +0530

    Additional Params to pass to the modal Dialog
    Additional Params to pass to the modal Dialog for Share

--- web/studio/widgets/directive/portal/sharePopup/share-content-popup.js
+++ web/studio/widgets/directive/portal/sharePopup/share-content-popup.js
@@ -1,27 +1,27 @@
 enablix.studioApp.factory('shareContentModalWindow', 
-			[	 '$state', '$stateParams', 'Notification', '$modal',
-	 	function( $state,   $stateParams,   Notification,   $modal) {
-	
-	
-		
-	var showShareContentModal = function(containerQId, contentIdentity) {
+		[	 '$state', '$stateParams', 'Notification', '$modal',
+			function( $state,   $stateParams,   Notification,   $modal) {
+
+			var showShareContentModal = function(containerQId, contentIdentity,$event) {
 				var modalInstance = $modal.open({
-				      templateUrl: 'widgets/directive/portal/sharePopup/share-content-popup.html',
-				      size: 'sm', // 'sm', 'lg'
-				      controller: 'ShareController',
-					  resolve: {				    	  
-						  containerQId: function() {
-				    		  return containerQId;
-				    	  },
-				    	  contentIdentity : function() {
-				    		  return contentIdentity;
-				    	  }
-					  }
+					templateUrl: 'widgets/directive/portal/sharePopup/share-content-popup.html',
+					size: 'sm', // 'sm', 'lg'
+					controller: 'ShareController',
+					parent: angular.element(document.body),
+					targetEvent: $event,
+					clickOutsideToClose:true,
+					resolve: {				    	  
+						containerQId: function() {
+							return containerQId;
+						},
+						contentIdentity : function() {
+							return contentIdentity;
+						}
+					}
 				});
 			};
+			return {
+				showShareContentModal : showShareContentModal
+			};	
 
-	return {
-		showShareContentModal : showShareContentModal
-	};	
-			
-}]);
+		}]);
