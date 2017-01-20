42cc47282c4532d95ee8c80cba355528086639a8
amjayee <amit.jayee@gmail.com>
2017-01-20 11:43:33 +0530

    Removed getEmail content

--- services/enablix-app/src/main/java/com/enablix/app/mail/ShareEmailService.java
+++ services/enablix-app/src/main/java/com/enablix/app/mail/ShareEmailService.java
@@ -1,10 +1,8 @@
 package com.enablix.app.mail;
 
 import com.enablix.app.mail.web.EmailData;
-import com.enablix.core.mail.entities.ShareEmailClientDtls;
 
 public interface ShareEmailService {
 
 	public boolean sendEmail(EmailData data);
-	public ShareEmailClientDtls getEmailContent(String containerQID,String contentID);
 }
