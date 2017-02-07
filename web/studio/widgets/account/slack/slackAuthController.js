619db4ac233c9836d263aa9626c44821ce262408
amjayee <amit.jayee@gmail.com>
2017-02-07 23:49:53 +0530

    Added a custom redirect URI

--- web/studio/widgets/account/slack/slackAuthController.js
+++ web/studio/widgets/account/slack/slackAuthController.js
@@ -1,6 +1,6 @@
 enablix.studioApp.controller('slackAuthController', 
-		['$scope', '$state', '$rootScope', 'RESTService', 'StateUpdateService','$stateParams',
-			function( $scope,   $state,   $rootScope,   RESTService,   StateUpdateService,$stateParams) {
+		['$scope', '$state', '$rootScope', 'RESTService', 'StateUpdateService','$stateParams','$location',
+			function( $scope,   $state,   $rootScope,   RESTService,   StateUpdateService,$stateParams,$location) {
 
 			var source = $stateParams.source;
 			var checkIfAlreadyAuth= function(){
@@ -15,8 +15,10 @@ enablix.studioApp.controller('slackAuthController',
 			}
 
 			$scope.authorizeSlack = function(){
+				var redirectURI="redirect_uri="+$location.protocol() + "://" + $location.host() 
+				+ ":" + $location.port() + "/app.html%23/account/slackdtls";
 				var url = enablix.serviceURL["getSlackCode"];
-				window.location.href=url ;
+				window.location.href=url +"&"+redirectURI;
 			}
 			if(source==null)
 				checkIfAlreadyAuth();
