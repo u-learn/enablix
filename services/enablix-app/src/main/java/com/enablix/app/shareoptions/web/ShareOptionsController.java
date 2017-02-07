3d57b5df1108ff8b4091c10c528a591560cad0a6
amjayee <amit.jayee@gmail.com>
2017-02-07 23:01:38 +0530

    Code Formatting

--- services/enablix-app/src/main/java/com/enablix/app/shareoptions/web/ShareOptionsController.java
+++ services/enablix-app/src/main/java/com/enablix/app/shareoptions/web/ShareOptionsController.java
@@ -22,48 +22,45 @@ import com.enablix.services.util.ActivityLogger;
 @RestController
 @RequestMapping("shareOptions")
 public class ShareOptionsController {
+	
 	private static final Logger LOGGER = LoggerFactory.getLogger(ShareOptionsController.class);
+	
 	@Autowired
 	private UUIDIdentityGenerator uuidGen;
 
 	@RequestMapping(method = RequestMethod.POST, value = "/downldDocURLAudit")
 	public @ResponseBody Boolean auditDownldDoc(@RequestBody ContentAuditRequest request) {
-		try
-		{
+		try	{
 			String templateId = ProcessContext.get().getTemplateId();
 			ContentDataRef dataRef = new ContentDataRef(templateId, request.getContainerQId(), 
 					request.getInstanceIdentity(), request.getItemTitle());
 			ActivityLogger.auditContentDownldURLCopied(dataRef, ContentActivity.ContainerType.CONTENT, Channel.WEB);
 			return true;
 		}
-		catch(Exception e)
-		{
-			LOGGER.error("Error Saving the Audit Data for the Activity :: Portal URL Copied");
+		catch(Exception e) {
+			LOGGER.error("Error Saving the Audit Data for the Activity :: Portal URL Copied",e);
 			return false;
 		}
 	}
 	
 	@RequestMapping(method = RequestMethod.POST, value = "/portalURLAudit")
 	public @ResponseBody Boolean auditPortalURL(@RequestBody ContentAuditRequest request) {
-		try
-		{
+		try	{
 			String templateId = ProcessContext.get().getTemplateId();
 			ContentDataRef dataRef = new ContentDataRef(templateId, request.getContainerQId(), 
 					request.getInstanceIdentity(), request.getItemTitle());
 			ActivityLogger.auditContentPortalURLCopied(dataRef, ContentActivity.ContainerType.CONTENT, Channel.WEB);
 			return true;
 		}
-		catch(Exception e)
-		{
-			LOGGER.error("Error Saving the Audit Data for the Activity :: Portal URL Copied");
+		catch(Exception e) {
+			LOGGER.error("Error Saving the Audit Data for the Activity :: Portal URL Copied",e);
 			return false;
 		}
 	}
 
 	@RequestMapping(value = "/auditShareViaEmailClient", method = RequestMethod.POST, produces = "application/json")
 	public @ResponseBody Boolean auditShareViaEmailClient(@RequestBody ContentAuditRequest request) {
-		try
-		{
+		try	{
 			String templateId = ProcessContext.get().getTemplateId();
 			String uniqueShareId = uuidGen.generateId(null);
 			ActivityLogger.auditContentShareInternal(templateId, request.getInstanceIdentity(),request.getContainerQId(),"",
@@ -71,11 +68,10 @@ public class ShareOptionsController {
 
 			return true;
 		}
-		catch(Exception e)
-		{
-			LOGGER.error("Error Saving the Audit Data for the Activity :: Portal URL Copied");
+		catch(Exception e)	{
+			LOGGER.error("Error Saving the Audit Data for the Activity :: Portal URL Copied",e);
 			return false;
 		}
-	
 	}
+	
 }
