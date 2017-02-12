ac0236283bb29636a81b856f59db1df315054f37
amjayee <amit.jayee@gmail.com>
2017-02-12 23:00:03 +0530

    Slack Dtls from ConfigurationProvider

--- commons/commons-mongo/src/main/java/com/enablix/core/system/repo/SlackAppDtlsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.enablix.core.system.repo;
-
-import com.enablix.core.domain.slackdtls.SlackAppDtls;
-import com.enablix.core.mongo.repository.BaseMongoRepository;
-
-public interface SlackAppDtlsRepository extends BaseMongoRepository<SlackAppDtls> {
-	SlackAppDtls findByAppName(String appName);
-}
