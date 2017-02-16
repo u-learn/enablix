02692f747cdd7defeaaeb1e437edd119bf94ff4b
amjayee <amit.jayee@gmail.com>
2017-02-17 00:20:20 +0530

    Channel Validation

--- web/studio/widgets/directive/portal/sharePopup/shareSlack/shareToSlack.js
+++ web/studio/widgets/directive/portal/sharePopup/shareSlack/shareToSlack.js
@@ -1,7 +1,7 @@
 enablix.studioApp.controller('ShareToSlackController', [
 	'$scope', '$stateParams', '$modalInstance', '$timeout', 'containerQId', 'contentIdentity', 'ContentShareService', 'Notification','RESTService',
 	function( $scope,   $stateParams,   $modalInstance,   $timeout,   containerQId,   contentIdentity,   ContentShareService,   Notification,RESTService) {
-		
+
 		$scope.channelLst=[];
 		$scope.slackCustomContent="";
 		function getChannels(_success){
@@ -11,7 +11,7 @@ enablix.studioApp.controller('ShareToSlackController', [
 				Notification.error({message: "Error loading channels ", delay: enablix.errorMsgShowTime});
 			});
 		};
-		
+
 		function populateChannels(data){
 			var channelsDtls = data.channels;
 			if(channelsDtls!=null && channelsDtls!=undefined){
@@ -26,19 +26,32 @@ enablix.studioApp.controller('ShareToSlackController', [
 				}
 			}
 		}
-
+		function validateChannel() {
+			if($scope.channelID==null || $scope.channelID==undefined) {
+				return false;
+			}
+			else {
+				return true;	
+			}
+		}
 		$scope.shareContent = function() {
-			ContentShareService.shareContentToSlack(containerQId, contentIdentity, $scope.channelID,$scope.slackCustomContent,
-					function(sent) {
-				if(sent) {
-					Notification.primary({message: "Content Shared successfully", delay: enablix.errorMsgShowTime});
-				} else {
-					Notification.error({message: "Error sending content to Slack ", delay: enablix.errorMsgShowTime});
-				}
-			}, function(fail) {
-				Notification.error({message: "Error sending content to Slack ", delay: enablix.errorMsgShowTime});
-			});
-			$modalInstance.close();
+			var valResp = validateChannel();
+			if(valResp) {
+				ContentShareService.shareContentToSlack(containerQId, contentIdentity, $scope.channelID,$scope.slackCustomContent,
+						function(sent) {
+					if(sent) {
+						Notification.primary({message: "Done! Shared successfully on Slack.", delay: enablix.errorMsgShowTime});
+					} else {
+						Notification.error({message: "Error sending content to Slack. ", delay: enablix.errorMsgShowTime});
+					}
+				}, function(fail) {
+					Notification.error({message: "Error sending content to Slack. ", delay: enablix.errorMsgShowTime});
+				});
+				$modalInstance.close();
+			}
+			else{
+				Notification.error({message: "Please select a Channel.", delay: 3000});
+			}
 		}
 
 		$scope.close = function() {
