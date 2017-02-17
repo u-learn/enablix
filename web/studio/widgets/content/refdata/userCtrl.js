fc2d4698e6ac86053ec5ad855988c0ae4830f980
amjayee <amit.jayee@gmail.com>
2017-02-17 23:55:45 +0530

    confirmation dialog for delete

--- web/studio/widgets/content/refdata/userCtrl.js
+++ web/studio/widgets/content/refdata/userCtrl.js
@@ -1,6 +1,6 @@
 enablix.studioApp.controller('UserController', 
-			['$scope', '$state', 'RESTService','UserService', '$rootScope', 'StateUpdateService',
-	function( $scope,   $state,   RESTService,  UserService,   $rootScope,   StateUpdateService) {
+			['$scope', '$state', 'RESTService','UserService', '$rootScope', 'StateUpdateService', 'ConfirmationModalWindow',
+	function( $scope,   $state,   RESTService,  UserService,   $rootScope,   StateUpdateService, ConfirmationModalWindow) {
 		 
 		$scope.breadcrumbList = 
 		[
@@ -30,10 +30,19 @@ enablix.studioApp.controller('UserController',
 					 return obj;
 			 });
 			 
-			 var userTodelete=user[0]; 
-			 UserService.deleteUser(userTodelete,function(data){
-				 init();
-			 });			 
+			var userTodelete=user[0]; 
+			var confirmModal = ConfirmationModalWindow.showWindow("Confirm", 
+					"Do you want to continue?", 
+					"Proceed", "Cancel");
+
+			confirmModal.result.then(function(confirmed) {
+
+				if (confirmed) {
+					UserService.deleteUser(userTodelete,function(data){
+						init();
+					});
+				}
+			});			 
 		 }
 		 
 		 init();
