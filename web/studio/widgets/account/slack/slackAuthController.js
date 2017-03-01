1af630b6ddddf80b60d56360720acc993c5d0bdc
amjayee <amit.jayee@gmail.com>
2017-03-02 00:04:56 +0530

    Slack User Name

--- web/studio/widgets/account/slack/slackAuthController.js
+++ web/studio/widgets/account/slack/slackAuthController.js
@@ -7,7 +7,7 @@ enablix.studioApp.controller('slackAuthController',
 				
 				RESTService.getForData('getSlackStoredAuthAccessToken', null, null, function(data) {
 					if( data != null && data.accessToken!=null && data.teamName!=null ){
-						StateUpdateService.goToSlackDtls(data.teamName);
+						StateUpdateService.goToSlackDtls(data.teamName,data.slackUserID);
 					}
 				}, function() {    		
 					Notification.error({message: "Oops! There was an error on Slack.", delay: enablix.errorMsgShowTime});
@@ -16,7 +16,7 @@ enablix.studioApp.controller('slackAuthController',
 
 			$scope.authorizeSlack = function(){
 				var redirectURI="redirect_uri="+$location.protocol() + "://" + $location.host() 
-				 + "/app.html%23/account/slackdtls";
+				+ "/app.html%23/account/slackdtls";
 				var url = enablix.serviceURL["getSlackCode"];
 				window.location.href=url +"&"+redirectURI;
 			}
