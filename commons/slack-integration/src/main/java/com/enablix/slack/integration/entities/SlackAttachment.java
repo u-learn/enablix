415ace6cb4f3294967f470a0b1ad602689494478
amjayee <amit.jayee@gmail.com>
2017-03-01 23:59:41 +0530

    Slack User Name

--- commons/slack-integration/src/main/java/com/enablix/slack/integration/entities/SlackAttachment.java
+++ commons/slack-integration/src/main/java/com/enablix/slack/integration/entities/SlackAttachment.java
@@ -15,6 +15,8 @@ public class SlackAttachment {
 
 	private String color;
 
+	private String text;
+	
 	private String title;
 
 	@JsonProperty("title_link")
@@ -24,10 +26,10 @@ public class SlackAttachment {
 	private String footerIcon;
 
 	private List<SlackAttachmentFields> fields;
-	
-	private SlackAttachment(String fallBack, String title, String titleLink, String footerIcon, String color,
-			String footerText,List<SlackAttachmentFields> fields) {
 
+	private SlackAttachment(String text, String fallBack, String title, String titleLink, String footerIcon, String color,
+			String footerText,List<SlackAttachmentFields> fields) {
+		this.text=text;
 		this.fallBack=fallBack;
 		this.title=title;
 		this.titleLink=titleLink;
@@ -37,6 +39,14 @@ public class SlackAttachment {
 		this.fields=fields;
 	}
 
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+	
 	public String getFooterIcon() {
 		return footerIcon;
 	}
@@ -92,7 +102,7 @@ public class SlackAttachment {
 	public void setFields(List<SlackAttachmentFields> fields) {
 		this.fields = fields;
 	}
-	
+
 	public static class SlackAttachmentBuidler {
 		private String fallBack;
 		private String title;
@@ -102,12 +112,12 @@ public class SlackAttachment {
 		private String color;
 		private String footerText;
 		private List<DisplayField<?>> fields;
-		
+
 		public SlackAttachmentBuidler fallBack(String fallBack)	{
 			this.fallBack = fallBack;
 			return this;
 		}
-		
+
 		public SlackAttachmentBuidler footerText(String footerText)
 		{
 			this.footerText=footerText;
@@ -117,8 +127,10 @@ public class SlackAttachment {
 			this.footerIcon = footerIcon;
 			return this;
 		}
-		public SlackAttachmentBuidler text(String text)	{
-			this.text = text;
+		
+		public SlackAttachmentBuidler text(String docName, String docURL)	{
+			if(docName!="" && docURL!= "")
+				this.text ="<"+docURL+"|"+docName+">";
 			return this;
 		}
 
@@ -127,47 +139,52 @@ public class SlackAttachment {
 			return this;
 		}
 
+		
+
 		public SlackAttachmentBuidler titleLink(String titleLink) {
 			this.titleLink = titleLink;
 			return this;
 		}
-		
+
 		public SlackAttachmentBuidler color(String color) {
 			this.color = color;
 			return this;
 		}
-		
+
 		public SlackAttachmentBuidler fields(List<DisplayField<?>> fields) {
 			this.fields = fields;
 			return this;
 		}
-		
+
 		public SlackAttachment build() {
 			// To be used later when we extend the Slack Attachment functionality
 			/*
 			List<SlackAttachmentFields> slackAttachLst = new LinkedList<SlackAttachmentFields>();
 			for (DisplayField<?> field : fields) {
-			
+
 				String value = processDisplayField(field);
-				
+
 				if (value != null && !value.isEmpty()){
 					SlackAttachmentFields slackAttcField = new SlackAttachmentFields(field.getLabel(), value);
 					slackAttachLst.add(slackAttcField);
 				}
 			}
-			*/
-			return new SlackAttachment(fallBack, title, titleLink, footerIcon, color, footerText, null);
+			 */
+			//List<SlackAttachmentFields> slackFieldAttachLst = new ArrayList<SlackAttachmentFields>();
+			//slackFieldAttachLst.add(new SlackAttachmentFields(docName, docURL));
+			SlackAttachment slackAttac = new SlackAttachment(text, fallBack, title, titleLink, footerIcon, color, footerText, null);
+			return slackAttac;
 		}
-		
+
 		// To be used later when we extend the Slack Attachment functionality
 		/*
 		private String processDisplayField(DisplayField<?> field) {
-			
+
 			if (field.getValue() instanceof TextValue) {
 				TextValue txtVal = (TextValue) field.getValue();
 				return txtVal.getValue();
 			}
-		
+
 			return null;	
 		}*/
 	}
