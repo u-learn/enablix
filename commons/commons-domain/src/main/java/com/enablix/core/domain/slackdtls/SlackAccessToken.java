63970bc41a6a55595a503ccd26abf1b838bb8f9c
amjayee <amit.jayee@gmail.com>
2017-03-02 00:01:56 +0530

    Slack User Name

--- commons/commons-domain/src/main/java/com/enablix/core/domain/slackdtls/SlackAccessToken.java
+++ commons/commons-domain/src/main/java/com/enablix/core/domain/slackdtls/SlackAccessToken.java
@@ -6,30 +6,47 @@ import com.enablix.commons.constants.AppConstants;
 import com.enablix.core.domain.BaseDocumentEntity;
 @Document(collection = AppConstants.SYSTEM_SLACK_TOKEN)
 public class SlackAccessToken extends BaseDocumentEntity{
+	String slackUserID;
 	String userID;
 	String accessToken;
 	String teamName;
-	public String getTeamName() {
-		return teamName;
-	}
-	public void setTeamName(String teamName) {
-		this.teamName = teamName;
-	}
-	public SlackAccessToken(String userID,String accessToken,String teamName) {
+	
+	public SlackAccessToken(String userID,String accessToken,String teamName,String slackUserID) {
 		this.userID=userID;
 		this.accessToken = accessToken;
 		this.teamName=teamName;
+		this.slackUserID=slackUserID;
 	}
+	
+	public String getTeamName() {
+		return teamName;
+	}
+	
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+	
 	public String getAccessToken() {
 		return accessToken;
 	}
+	
 	public void setAccessToken(String accessToken) {
 		this.accessToken = accessToken;
 	}
+	
 	public String getUserID() {
 		return userID;
 	}
+	
 	public void setUserID(String userID) {
 		this.userID = userID;
+	}
+	
+	public String getSlackUserID() {
+		return slackUserID;
+	}
+	
+	public void setSlackUserID(String slackUserID) {
+		this.slackUserID = slackUserID;
 	}
 }
