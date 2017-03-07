474bbcde1407234950bd4890aa3ec4cae40d0b3d
dluthra <dluthra@dluthra-PC>
2017-03-07 23:53:15 +0530

    make set password screen visible after login

--- web/studio/js/common/login/setPasswordController.js
+++ web/studio/js/common/login/setPasswordController.js
@@ -1,6 +1,9 @@
 enablix.studioApp.controller('SetPasswordController', 
 			['$scope', '$state', 'RESTService','UserService', '$rootScope', 'StateUpdateService',
 	function( $scope,   $state,   RESTService,  UserService, $rootScope,   StateUpdateService) {
+		// make the body visible only after authentication is complete
+		$(document.body).css({"display": "block"});
+		
 		$scope.passwordData={};
 		$scope.setPasswordSubmit = function() {
 			if($scope.passwordData.confirm===$scope.passwordData.password){
