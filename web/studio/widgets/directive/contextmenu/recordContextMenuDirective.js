d6859080b9afc08d8792c2244aa8336ddca80988
dluthra <dluthra@dluthra-PC>
2017-04-20 16:49:35 +0530

    Fix File Options UI for doc with no preview option

--- web/studio/widgets/directive/contextmenu/recordContextMenuDirective.js
+++ web/studio/widgets/directive/contextmenu/recordContextMenuDirective.js
@@ -1,6 +1,6 @@
 enablix.studioApp.directive('ebxRecordContextMenu',
-			['RESTService', 'Notification', '$state', 'StateUpdateService',
-	function (RESTService,   Notification,   $state,   StateUpdateService) {
+			['RESTService', 'Notification', '$state', 'StateUpdateService', 'DocPreviewService',
+	function (RESTService,   Notification,   $state,   StateUpdateService,   DocPreviewService) {
 	return {
 		restrict: 'E',
 		scope : {
@@ -8,7 +8,9 @@ enablix.studioApp.directive('ebxRecordContextMenu',
 			contentQId : "="
 		},
 		link: function(scope, element, attrs) {
-			
+			if (scope.contentRecord.docMetadata) {
+				scope.hasPreviewOption = DocPreviewService.getPreviewHandler(scope.contentRecord.docMetadata) != null;
+			}
 		},
 		templateUrl: "widgets/directive/contextmenu/recordContextMenu.html"
 	};
