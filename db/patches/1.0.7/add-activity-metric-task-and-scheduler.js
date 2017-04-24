f3df9eb9a60edc0c59ee85745cd6e2e96d38ec16
dluthra <dluthra@dluthra-PC>
2017-04-24 14:28:37 +0530

    db patch for activity metric task and scheduler

---
+++ db/patches/1.0.7/add-activity-metric-task-and-scheduler.js
@@ -0,0 +1,27 @@
+/**************************************************************
+Script to add activity metric calculator task and also create a 
+1 AM GMT scheduler to run that task.
+
+Execute: use mongo utility to run the script
+
+mongo localhost:27017/admin -u enablix_app -p <password> <path>/<script-file-name>
+
+****************************************************************/
+
+// Switch to system database
+db = db.getSiblingDB("system_enablix");
+
+db.ebx_task_config.insert({ 
+    "identity" : "activity-metric-calculator-identity", 
+    "name" : "Activity Metric Calculator Task", 
+    "taskId" : "activity-metric-calculator"
+});
+
+db.ebx_scheduler_config.insert({ 
+    "identity" : "daily-1-am-gmt-scheduler", 
+    "name" : "Daily 1 AM GMT Scheduler", 
+    "cronExpression" : "0 0 1 1/1 * ?", 
+    "tasksToExecute" : [
+        "activity-metric-calculator"
+    ]
+});
