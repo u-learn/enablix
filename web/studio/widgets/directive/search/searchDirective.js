f8be42c88dc93640043844c25787bd9f60b0cbd1
dluthra <dluthra@dluthra-PC>
2017-06-06 16:58:37 +0530

    search UI changes

--- web/studio/widgets/directive/search/searchDirective.js
+++ web/studio/widgets/directive/search/searchDirective.js
@@ -1,35 +1,46 @@
 enablix.studioApp.directive('ebxSearchBox', [
-		'StateUpdateService',"$q",'DataSearchService','ContentTemplateService',
-		function(StateUpdateService,$q,DataSearchService,ContentTemplateService) {
+				 'StateUpdateService', '$q', 'DataSearchService', 'ContentTemplateService',
+		function (StateUpdateService,   $q,   DataSearchService,   ContentTemplateService) {
 			return {
 				restrict : 'E',
 				scope : {
 				},
 				link : function(scope, element, attrs) {
+					
 					var itemContainerList = ContentTemplateService.getPortalTopNavItemContainers();
-					var asyncData = {template:enablix.pTemplate};
+					
+					var asyncData = { template : enablix.pTemplate };
+					
 					itemContainerList.forEach(function(containerQId){
+						
 						var qualifiedId = containerQId.qualifiedId;
-						var value = DataSearchService.promiseContainerDataSearchResult(qualifiedId).then(function(container){
-							return _.get(container,"data.content");
-						});
+						
+						var value = DataSearchService.promiseContainerDataSearchResult(qualifiedId).then(
+										function(container){
+											return _.get(container,"data.content");
+										});
+						
 						asyncData[qualifiedId] = value;
 					});
+					
 					var mountPoint = "#enablix-search";
-					function onTrueEnter(payload){
+					
+					function onTrueEnter(payload) {
 						var node = payload.node;
 						var searchText = payload.query;
-						if(node && node.type === "BizDimEntSubNode"){
+					
+						if (node && node.type === "BizDimEntSubNode"){
+							
 							var uri = node.asUri();
-							StateUpdateService.goToPortalSubContainerList(uri.containerQId,uri.containerQId+"."+uri.subContainerQId,uri.parentIdentity);
-							return;
-						}else if(!isNullOrUndefined(searchText)  && searchText.length > 0){
+							StateUpdateService.goToPortalSubContainerList(uri.containerQId, 
+									uri.containerQId + "." + uri.subContainerQId, uri.parentIdentity);
+							
+						} else if (!isNullOrUndefined(searchText) && searchText.length > 0) {
 							StateUpdateService.goToPortalSearch(searchText);
-						}else{
-
-						}
+						} 					
 					}
-					enablix.searchComponent = new enablix.SearchComponent({mountPoint:mountPoint,asyncData:asyncData});
+					
+					enablix.searchComponent = new enablix.SearchComponent({ mountPoint : mountPoint, asyncData : asyncData });
 					enablix.searchComponent.onTrueEnter = onTrueEnter;
 				},
 				templateUrl : "widgets/directive/search/searchBox.html"
