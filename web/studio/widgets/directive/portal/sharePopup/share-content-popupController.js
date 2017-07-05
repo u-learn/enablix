71d1f9a31bf79149c741f05eb74e52c2588c1861
dluthra <dluthra@192.168.0.101>
2017-07-05 14:20:22 +0530

    share email not having user name in greetings

--- web/studio/widgets/directive/portal/sharePopup/share-content-popupController.js
+++ web/studio/widgets/directive/portal/sharePopup/share-content-popupController.js
@@ -1,28 +1,41 @@
 enablix.studioApp.controller('ShareController', [
-	'$scope', '$stateParams', '$modalInstance', '$timeout', 'containerQId', 'contentIdentity', 'ContentShareService', 'Notification','RESTService',
-	function( $scope,   $stateParams,   $modalInstance,   $timeout,   containerQId,   contentIdentity,   ContentShareService,   Notification,RESTService) {
-		$scope.validationCheck=false;
-		var email ;
-		$scope.querySearch   = querySearch;
-		function querySearch (query) {
+			 '$scope', '$stateParams', '$modalInstance', '$timeout', 'containerQId', 'contentIdentity', 'ContentShareService', 'Notification', 'RESTService',
+	function( $scope,   $stateParams,   $modalInstance,   $timeout,   containerQId,   contentIdentity,   ContentShareService,   Notification,   RESTService) {
+		
+				 $scope.validationCheck = false;
+		var email;
+		
+		$scope.querySearch = function (query) {
 			return query ? $scope.users.filter( createFilterFor(query) ) : $scope.users;
 		}
+		
 		function validateEmail() {
 			
-			if($scope.selectedItem==null)
-			{
-				email=$scope.searchText;
+			if ($scope.selectedItem == null) {
+				
+				email = $scope.searchText;
+				
+				var ltpos = email.indexOf("<");
+				var gtpos = email.indexOf(">");
+				
+				if (ltpos >= 0 && gtpos > 0) {
+					email = email.substr(ltpos + 1, gtpos - ltpos - 1);
+				}
+				
+			} else {
+				email = $scope.selectedItem.value;
 			}
-			else{
-				email=$scope.selectedItem.value;
-			}
+			
 			var atpos = email.indexOf("@");
 			var dotpos = email.lastIndexOf(".");
+			
 			if ( atpos<1 || dotpos<atpos+2 || dotpos+2>=email.length ) {
 				return false;
 			}
+			
 			return true;
 		}
+		
 		function getAllUsers(_success){
 			RESTService.getForData('systemuser', null, null, function(data) {
 				_success(data);	    	
@@ -30,40 +43,45 @@ enablix.studioApp.controller('ShareController', [
 				Notification.error({message: "Error loading user data", delay: enablix.errorMsgShowTime});
 			});
 		};
+		
 		function populateUserIds(data){
-			$scope.users = data.map(function (data) {
+			$scope.users = data.map(function (dataItem) {
 				return {
-					value: data.email.toLowerCase(),
-					display: (data.name+" <"+data.email.toLowerCase()+">")
+					value: dataItem.email.toLowerCase(),
+					display: (dataItem.name + " <" + dataItem.email.toLowerCase() + ">")
 				};
 			});
 		}
 
 		function createFilterFor(query) {
+			
 			var lowercaseQuery = angular.lowercase(query);
 
 			return function filterFn(state) {
 				return (state.value.indexOf(lowercaseQuery) === 0);
 			};
-
 		}
+		
 		$scope.shareContent = function() {
-			var val = validateEmail();
-			if(val)
-			{
-				ContentShareService.shareContent(containerQId, contentIdentity, email,$scope.emailContent ,function(sent) {
+			
+			var valid = validateEmail();
+			if (valid) {
+				
+				ContentShareService.shareContent(containerQId, contentIdentity, email, $scope.emailContent ,function(sent) {
+				
 					if(sent) {
 						Notification.primary({message: "Mail sent successfully", delay: enablix.errorMsgShowTime});
 					} else {
 						Notification.error({message: "Error sending mail ", delay: enablix.errorMsgShowTime});
 					}
+					
 				}, function(fail) {
 					Notification.error({message: "Error sending mail ", delay: enablix.errorMsgShowTime});
 				});
 
 				$modalInstance.close();
-			}
-			else{
+				
+			} else {
 				Notification.error({message: "Please enter a valid E-Mail ID ", delay: 3000});
 			}
 		}
@@ -71,5 +89,7 @@ enablix.studioApp.controller('ShareController', [
 		$scope.close = function() {
 			$modalInstance.dismiss('cancel');
 		}
+		
 		getAllUsers(populateUserIds);
+		
 	}]);
