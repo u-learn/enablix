d5126606af2e29d55bca6605236f6801b3630749
dluthra <dluthra@192.168.0.100>
2017-10-16 19:54:59 +0530

    file move error in dropbox when file exists in destination location

--- web/studio/widgets/directive/doc/fileUploadDirective.js
+++ web/studio/widgets/directive/doc/fileUploadDirective.js
@@ -16,7 +16,7 @@ enablix.studioApp.directive('ebFileUpload', function() {
 			scope.name = _dataDef.id;
 			scope.label = _dataDef.label;
 			scope.qualifiedId = _dataDef.qualifiedId
-			scope.temporary = true; // always temporary. will get moved when content is actually saved. //scope.temporary || false;
+			scope.temporary = scope.temporary || false; // will check and revert to true .. currently causing error in haystax while moving file when the destination file already exists in dropbox.. true; // always temporary. will get moved when content is actually saved. //scope.temporary || false;
 		},
 
 		controller: 'FileUploadCtrl',
