eecd759916ae271ad28cb21e5fd23c69a829bb8a
dluthra <dluthra82@gmail.com>
2017-11-15 18:32:56 +0530

    TCG Digital tenant setup

---
+++ db/tenant-config/tcgdigital-local-tenant-config.js
@@ -0,0 +1,5 @@
+var tenantId = "tcgdigital";
+var tenantName = "TCG Digital";
+var templateId = "tcgdigital_enterprise";
+var adminUserId = "dluthra82+tcgdigital@gmail.com";
+var adminUserName = "TCG Digital Admin";
---
+++ db/tenant-config/tcgdigital-test-tenant-config.js
@@ -0,0 +1,5 @@
+var tenantId = "tcgdigital";
+var tenantName = "TCG Digital";
+var templateId = "tcgdigital_enterprise";
+var adminUserId = "gaurav.harode+tcgdigital@enablix.com";
+var adminUserName = "Gaurav Harode";
