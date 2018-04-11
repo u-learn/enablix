89e51ceca22c0719c15a1a7b102e9ac62744ac3f
dluthra <dluthra82@gmail.com>
2018-04-11 15:41:37 +0530

    technomile tenant setup

---
+++ db/tenant-config/technomile-prod-tenant-config.js
@@ -0,0 +1,5 @@
+var tenantId = "technomile";
+var tenantName = "Technomile";
+var templateId = "technomile_enterprise";
+var adminUserId = "gaurav.harode+technomile@enablix.com";
+var adminUserName = "Gaurav Harode";
